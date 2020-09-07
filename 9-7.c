854d23e370ccec3ea1d16c33b9fe652f9c34903d
OldFishmaru <70792520+OldFishmaru@users.noreply.github.com>
2020-09-07 19:38:32 +0800

    Add files via upload

---
+++ 9-7.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum =%d\n", sum);
+//	 
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int num = 4;//const-常属性，修饰常变量
+//	printf("%d\n", num);
+//	num = 8;
+//	printf("%d\n", num);
+//	return 0;//不可执行
+//}
+
+//int main()
+//{
+//	const int n = 10;//n是变量但拥有常属性
+//	//使用常量的地方n不能做，去表达一个不能变的量可以
+//	int arr[n] = { 0 };
+//	return 0;
+//}
+//#define max 10//define定义的标识符常量
+//int main()
+//{
+//	int arr[max] = { 0 };
+//	printf("%d\n", max);
+//	return 0;
+//
+//}
+
+
+//枚举关键-enum
+//enum sex 
+//{ male,//枚举常量不能改
+// female,
+// secret
+//};
+//int main()
+//{
+//	printf("%d\n", male);//0
+//	printf("%d\n", female);//1
+//	printf("%d\n", secret);//2
+//	return 0;
+
+}
