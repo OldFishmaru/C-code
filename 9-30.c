813a77db55685a5d832ada8c84afab8fdd907b7b
OldFishmaru <70792520+OldFishmaru@users.noreply.github.com>
2020-09-30 22:48:28 +0800

    Add files via upload

---
+++ 9-30.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+
+//ÇónµÄ½×³Ë
+//int fac(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n*fac(n - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = fac(n);
+//	printf("%d", ret);
+//	return 0;
+//}
